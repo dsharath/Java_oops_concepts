ca03532a61ec9daf01482d479cc2a5bef556dd6e
SHARATH CHANDRA <dsharathc7@gmail.com>
2019-08-11 17:37:11 -0400

    Java: Static and Instance contrl flow

---
+++ InstanceStatic.java
@@ -0,0 +1,33 @@
+package snippet;
+/* 
+ * Class contains the constructor, static and 
+ * Instance control flows in the java
+ * 
+ */
+public class InstanceStatic {
+	
+	{
+		System.out.println("First Instance Block...");
+	}
+	
+	static {
+		System.out.println("Fisrt Static Block...");
+	}
+	InstanceStatic(){
+		System.out.println("Constructor...");
+	}
+
+	public static void main(String[] args) {
+
+		InstanceStatic is1 = new InstanceStatic();
+		System.out.println("Main Method...");
+		InstanceStatic is2 = new InstanceStatic();
+	}
+
+	static {
+		System.out.println("Second Static Method...");
+	}
+	{
+		System.out.println("Second Static Methdo...");
+	}
+}
